6e441c51b003b564bdf1241b69a752cfacdf92c4
crelluc <lucien.crelier@divtec.ch>
2023-09-05 11:59:16 +0200

    transformation de °C en °F

--- js/main.js
+++ js/main.js
@@ -12,4 +12,16 @@ let a = 'Bonjour';
 
 console.log(a);
 
+// Demander la température en celsius
+let tempCelsius = prompt('Température en celius :');
 
+// Transformer la température en nombre entier
+tempCelsius = parseInt(tempCelsius);
+
+// Tester si c'est un nombre
+if (isNaN(tempCelsius)) {
+    alert('Erreur, entre une température !');
+} else {
+    // affiche la température en Fahrenheit
+    alert(`${tempCelsius}°C = ${tempCelsius * 9 / 5 + 32}°F`);
+}
